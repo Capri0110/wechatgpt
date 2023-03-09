241976ab87f1035356d52214d4c4221e5d4819df
nirvana.xu <emberxu_fcphy@163.com>
2023-03-09 23:40:28 +0800

    [2nd] Commit

--- src/main/java/com/nirvana/wechatgpt/WechatgptApplication.java
+++ src/main/java/com/nirvana/wechatgpt/WechatgptApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author honghu
+ * @author nirvana
  */
 @SpringBootApplication
 public class WechatgptApplication {
